71065be89a20cacb79b67df9dbf6ce9d0780ada0
ser-jun <svyatokhos@mail.ru>
2024-10-03 18:51:38 +0300

    Добавьте файлы проекта.

---
+++ Guessing game/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+namespace Guessing_game
+{
+    public partial class MainWindow : Window
+    {
+        private int lowerBound = 1;
+        private int upperBound = 1000;
+        private int guess;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MessageBox.Show("Задумайте число от 1 до 1000!", "Угадайка");
+            GuessingNumber();
+        }
+
+        private void GuessingNumber()
+        {
+            guess = (lowerBound + upperBound) / 2;
+
+            MessageBoxResult result = MessageBox.Show($"Ваше число — это {guess}?", "Угадайка", MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes) { MessageBox.Show("Я угадал!", "Угадайка"); Close(); }
+
+            else
+            {
+
+                MessageBoxResult moreLess = MessageBox.Show($"Ваше число больше {guess}?", "Угадайка", MessageBoxButton.YesNo);
+
+                if (moreLess == MessageBoxResult.Yes) { lowerBound = guess + 1; }
+
+                else { upperBound = guess - 1; }
+
+
+                GuessingNumber();
+            }
+        }
+    }
+}
